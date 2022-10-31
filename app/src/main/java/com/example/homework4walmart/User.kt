870d814dcebf2c5660b95b416c3cb137878b5838
Temuuhei <temuujintsogt@gmail.com>
2022-10-30 19:09:35 -0500

    Initial commit

---
+++ app/src/main/java/com/example/homework4walmart/User.kt
@@ -0,0 +1,6 @@
+package com.example.homework4walmart
+import java.io.Serializable;
+
+class User(var firstName: String = "", var lastName: String = "", var userName: String = "", var password: String = "") : Serializable {
+
+}
